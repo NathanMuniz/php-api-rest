718684798f13bef6f1dade978288df392be54494
NathanMuniz <natangabriel.palmeiras@gmail.com>
2023-04-07 08:27:36 -0300

    Trying to execute the api

---
+++ build-a-restfull-api-in-php/index.php
@@ -0,0 +1,10 @@
+<?php
+
+$URI = parse_url($_SERVER['REQUEST_URI'])['path'];
+
+$path = explode('/', $URI)[1];
+
+if ($path === 'api')
+  include_once './api.php';
+else
+  include_once './viws/index.html';
